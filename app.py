bd4b1223bfb41e741b9dbdc954badaa27ba69433
heylovekush <officialloviie@gmail.com>
2026-01-27 22:53:32 +0530

    add app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Instagram Reel Downloader Live"
+
+@app.route("/download", methods=["POST"])
+def download():
+    url = request.json["url"]
+
+    with yt_dlp.YoutubeDL({"quiet": True}) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return jsonify({"video": info["url"]})
+
+app.run(host="0.0.0.0", port=10000)
