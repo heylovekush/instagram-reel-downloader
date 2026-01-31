15de4ac13a22e5e125af45dbaebe43b3f3ce70e3
heylovekush <officialloviie@gmail.com>
2026-01-31 22:26:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, send_file
 import yt_dlp
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
 @app.route("/download", methods=["POST"])
 def download():
-    url = request.json["url"]
-    with yt_dlp.YoutubeDL({"quiet": True}) as ydl:
-        info = ydl.extract_info(url, download=False)
-        return jsonify({"video": info["url"]})
+    url = request.form["url"]
 
-app.run(host="0.0.0.0", port=10000)
+    filename = "video.mp4"
+
+    ydl_opts = {
+        "outtmpl": filename,
+        "quiet": True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return send_file(filename, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
